99d0b44967f3052f00d6bfd117b713aba6067ef1
InkaOro <65492226+InkaOro@users.noreply.github.com>
2022-05-24 11:26:41 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :dogs do
-    resources :bookings, only: [:new, :create]
-  end
-  resources :bookings, except: [:new, :create]
+  resources :dogs
+  # do
+  #   resources :bookings, only: [:new, :create, :show]
+  # end
+  # resources :bookings, except: [:new, :create]
 end
