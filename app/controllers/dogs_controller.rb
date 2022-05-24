d453aeb68d11081e0c2b4b6060dd32a478819525
Nathalie Roth <nathaliemiriam.roth@gmail.com>
2022-05-24 13:13:19 +0100

    updated the controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,52 +1,50 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:show, :edit, :update, :destroy]
+
   def index
+    # @dogs = policy_scope(Dog)
     @dogs = Dog.all
   end
 
   def new
     @dog = Dog.new
-    authorize @dog
+    # authorize @dog
   end
 
   def create
     @rdog = Dog.new(dog_params)
-    @dog.user = current_user
-    authorize @dog
+    # @dog.user = current_user
+    # authorize @dog
 
     if @dog.save
-      redirect_to @rdog, notice: 'Dog was successfully created.'
+      redirect_to @dog, notice: 'Dog was successfully created.'
     else
       render :new
     end
   end
 
   def show
-    set_dog
-    authorize @dog
   end
 
   def update
     @dog.update(dog_params)
     redirect_to dog_path(@dog), notice: 'Dog was successfully updated.'
-    authorize @dog
   end
 
   def edit
-    set_dog
-    authorize @dog
+
   end
 
   def destroy
-    set_dog
     @dog.destroy
     redirect_to dogs_path, notice: 'Dog was successfully deleted.'
-    authorize @dog
   end
 
   private
 
   def set_dog
     @dog = Dog.find(params[:id])
+    # authorize @dog
   end
 
   def dog_params
